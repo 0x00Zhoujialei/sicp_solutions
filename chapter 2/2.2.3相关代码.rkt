8196da6668265f0c64355ae9cc3bd8eadc32a9cb
zhoujialei <294623884@qq.com>
2018-05-18 11:43:37 +0800

    2.3.3相关代码 - nested mappings

--- chapter 2/2.2.3相关代码.rkt
+++ chapter 2/2.2.3相关代码.rkt
@@ -99,3 +99,37 @@
               (map square
                    (filter odd? seq))))
 (product-of-squares-of-odd-elements (list 1 2 3 4 5))
+
+; nested mappings
+(define (prime? num)
+  (define (helper count)
+    (cond ((> (square count) num) #t)
+          ((= 0 (remainder num count)) #f)
+          (else (helper (+ count 2)))))
+  (if (= 0 (remainder num 2))
+      #f
+      (helper 3)))
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair)
+                                  (cadr pair))))
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (flatmap
+                (lambda (i)
+                  (map (lambda (j) (list i j))
+                       (enumerate-interval 1 (- i 1))))
+                  (enumerate-interval 1 n)))))
+(prime-sum-pairs 6)
+
+(define (permutations s)
+  (if (null? s)
+      (list nil)
+      (flatmap (lambda (x)
+                 (map (lambda (p) (cons x p))
+                      (permutations (remove x s))))
+               s)))
