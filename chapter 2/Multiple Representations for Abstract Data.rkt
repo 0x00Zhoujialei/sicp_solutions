d57cea88cad60a878fc075c468aaca088d59d9ab
zhoujialei <294623884@qq.com>
2018-08-07 01:23:35 +0800

    exercise 2.73

--- chapter 2/Multiple Representations for Abstract Data.rkt
+++ chapter 2/Multiple Representations for Abstract Data.rkt
@@ -28,7 +28,7 @@
 
 (define (rectangular? z)
   (eq? (type-tag z) 'rectangular))
-(define (polar z) (eq? (type-tag z) 'polar))
+(define (polar? z) (eq? (type-tag z) 'polar))
 
 (define (real-part-rectangular z) (car z))
 (define (imag-part-rectangular z) (cdr z))
@@ -81,3 +81,80 @@
         ((polar? z)
          (angle-polar (contents z)))
         (else (error "Unknown type: ANGLE" z))))
+
+(define (make-from-real-imag real imag)
+  (make-from-real-imag-rectangular real imag))
+(define (make-from-mag-ang magnitude angle)
+  (make-from-mag-ang-polar magnitude angle))
+(define (square x) (* x x))
+
+; 2.73
+; a
+; deriv函数事实上建立了以product, sum等为types轴，以deriv操作本身为operations轴的表
+; 对于number和variable类型之外的表达式使用data-directed programming technique来处理
+; number?是对于schema的基础类型数字类型的判断，并不需要我们封装相关逻辑来进行处理
+; variable? 函数传入的参数类型为基础类型symbol，对于这一基础类型的逻辑处理也不需要我们封装相关逻辑来处理
+
+; b
+(define (=number? exp num)
+    (and (number? exp) (= exp num)))
+(define (make-sum a1 a2)
+    (cond ((=number? a1 0) a2)
+          ((=number? a2 0) a1)
+          ((and (number? a1) (number? a2)) (+ a1 a2))
+          (else (list '+ a1 a2))))
+(define (make-product p1 p2)
+    (cond ((or (=number? p1 0) (=number? p2 0)) 0)
+          ((=number? p1 1) p2)
+          ((=number? p2 1) p1)
+          ((and (number? p1) (number? p2)) (* p1 p2))
+          (else (list '* p1 p2))))
+(define (make-exponentiation base pwr)
+  (cond ((=number? pwr 0) 1)
+        ((=number? pwr 1) base)
+        ((and (=number? base 0) (=number? pwr 0))
+         (error "no meaning!"))
+        ((=number? base 0) 0)
+        ((and (number? base) (number? pwr))
+         (expt base pwr))
+        (else
+         (list '** base pwr))))
+
+(define (install-sum-package)
+  (define (addend exp) (cadr exp))
+  (define (augend exp) (caddr exp))
+  (define (deriv exp var)
+    (make-sum (deriv (addend exp) var)
+              (deriv (augend exp) var)))
+  ;(put 'deriv '+ deriv)
+  'done)
+
+(define (install-product-package)
+  (define (multiplicand exp) (cadr exp))
+  (define (multiplier exp) (caddr exp))
+  (define (deriv exp var)
+    (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+  ;(put 'deriv '* deriv)
+  'done)
+
+; c
+
+(define (install-exponent-package)
+  (define (base expn) (cadr expn))
+  (define (exponent expn) (caddr expn))
+  (define (deriv exp var)
+    (make-product (exponent exp)
+                       (make-product (make-exponentiation (base exp)
+                                                         (make-sum (exponent exp)
+                                                                   '-1))
+                                     (deriv (base exp) var))))
+  ;(put 'deriv '** deriv)
+  'done)
+
+; d
+; 我的理解，derivative system不需要更改，只需要install一个包含对sum、product等进行
+; deriv操作的package即可
